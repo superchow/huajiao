e819085e8e8307d97020ecf64bbf558d8b5c70e4
superchow <2665752129@qq.com>
2017-06-07 13:23:28 +0800

    add
    添加响应头

--- DB/db.php
+++ DB/db.php
@@ -1,6 +1,5 @@
 <?php
-	header( "Access-Control-Allow-Origin:*");            
-	header('Access-Control-Allow-Methods:POST');    
+	header( "Access-Control-Allow-Origin:*");               
 // 响应头设置    
 	header('Access-Control-Allow-Headers:x-requested-with,content-type');  
 	class DB{
